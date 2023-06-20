3f6f01a204832ad476a1aa3a7c6c9e680aa649a8
likhith123-code <likhith2901@gmail.com>
2023-06-20 10:45:42 +0530

    view more added

--- src/Items/Item.js
+++ src/Items/Item.js
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 
 const Item = (props) => {
     console.log(props.data);
-    // const truncatedContent = props.data.data.slice(0, 200);
+    const truncatedContent = props.data.data.slice(0, 400);
     console.log(props);
     return (
         <div class="card">
             <div class="container">
                 <h3> Score: {props.data.score} </h3>
                 <p> {props.data.data}</p>
-                {/* <Link to={{ pathname: `/searchResults/${props.data.id}`, data: props.data }}>...Read More</Link> */}
+                <Link to={{ pathname: `/searchResults/${props.data.id}`, data: props.data }}>...Read More</Link>
             </div>
         </div>
     )
